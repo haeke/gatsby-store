a5449006d5825530d364ca6f2f1269df19e5f5b9
Edwin Rivera <waehake@gmail.com>
2019-04-01 09:24:48 -0400

    changed the navlink component

--- src/components/globals/navbar.js
+++ src/components/globals/navbar.js
@@ -17,12 +17,40 @@ const NavBar = () => {
       text: "about",
     },
   ]
-  const [navbarOpen, updateNavBar] = useState(false)
+  console.log(links)
+  const [navBarOpen, updateNavBar] = useState(false)
   const [cssClasses, updateCSSClasses] = useState("collapse navbar-collapse")
-  const [navBarLinks, updateLinks] = useState[links]
+  const [navBarLinks, updateLinks] = useState(links)
+  function toggleNavBar() {
+    console.log("navbar open value: ", navBarOpen)
+    updateNavBar(!navBarOpen)
+  }
   return (
-    <nav>
-      <h2>Nav Content</h2>
+    <nav className="navbar navbar-expand-sm bg-light navbar-light">
+      <Link to="/" className="navbar-brand">
+        <img src={logo} alt="logo" />
+        {/* https://www.iconfinder.com/icons/185113/coffee_streamline_icon
+            Creative Commons (Attribution 3.0 Unported);
+            https://www.iconfinder.com/webalys */}
+      </Link>
+      <button
+        className="navbar-toggler navbar-light"
+        type="button"
+        onClick={toggleNavBar}
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className={`${cssClasses}`}>
+        <ul className="navbar-nav mx-auto">
+          {navBarLinks.map(navLink => (
+            <li className="nav-item" key={navLink.id}>
+              <Link className="nav-link text-capitalize" to={navLink.path}>
+                {navLink.text}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </nav>
   )
 }
