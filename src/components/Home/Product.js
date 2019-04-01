d37fa93a648f7bf56e36d980e8fbaf44ecdf7858
Edwin Rivera <waehake@gmail.com>
2019-04-01 17:07:43 -0400

    added a product and products component.

--- src/components/Home/Product.js
+++ src/components/Home/Product.js
@@ -1,9 +1,20 @@
 import React from "react"
 
-const Product = () => {
+import Image from "gatsby-image"
+
+const Product = ({ product }) => {
   return (
-    <div>
-      <h1>The product component</h1>
+    <div className="col-10 col-sm-8 col-md-6 col-lg-4 mx-auto my-3">
+      <div className="card" style={{ minHeight: "100%" }}>
+        <Image fluid={product.image.fluid} className="card-img-top" />
+        <div className="card-body text-center">
+          <h6>{product.title}</h6>
+          <h6 className="text-yellow">${product.price}</h6>
+          <button type="button" className="btn btn-yellow mt-3 text-capitalize">
+            Add To Cart
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Home/Products.js
+++ src/components/Home/Products.js
@@ -1,10 +1,48 @@
 import React from "react"
 
+import Product from "./Product"
+import Title from "../Globals/Title"
+import { StaticQuery, graphql } from "gatsby"
+
+const getProducts = graphql`
+  {
+    products: allContentfulCoffeeProduct {
+      edges {
+        node {
+          id
+          title
+          price
+          image {
+            fluid(maxHeight: 420) {
+              ...GatsbyContentfulFluid_tracedSVG
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
 const Products = () => {
   return (
-    <div>
-      <Product />
-    </div>
+    <StaticQuery
+      query={getProducts}
+      render={data => {
+        return (
+          <section className="py-5">
+            <div className="container">
+              <Title title="Products" />
+              <div className="row">
+                {/* the list of products */}
+                {data.products.edges.map(({ node: product }) => (
+                  <Product key={product.id} product={product} />
+                ))}
+              </div>
+            </div>
+          </section>
+        )
+      }}
+    />
   )
 }
 
