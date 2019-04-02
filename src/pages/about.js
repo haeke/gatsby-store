7482894a7f421fb03adfb2c460e3424a2a0944f9
Edwin Rivera <waehake@gmail.com>
2019-04-02 10:39:57 -0400

    changed the Info component import statement inside the about component

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BackgroundSection from "../components/globals/BackgroundSection"
-import Info from "../components/Home/info"
+import Info from "../components/Home/Info"
 
 const AboutPage = ({ data }) => (
   <Layout>
