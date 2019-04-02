2d290e84332257fb4f7d6dd0ce26efeba1bd0935
Edwin Rivera <waehake@gmail.com>
2019-04-02 08:52:53 -0400

    changed the import statement for the Title component inside of the products component

--- src/components/Home/Products.js
+++ src/components/Home/Products.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 import Product from "./Product"
-import Title from "../Globals/Title"
+import Title from "../globals/Title"
 import { StaticQuery, graphql } from "gatsby"
 
 const getProducts = graphql`
