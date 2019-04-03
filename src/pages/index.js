80a04209d2d6095eb060d2211f2b4c2ddab4a8c1
Edwin Rivera <waehake@gmail.com>
2019-04-03 14:04:51 -0400

    removed the info import from the index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BackgroundSection from "../components/globals/BackgroundSection"
-import Info from "../components/Home/Info"
+// import Info from "../components/Home/Info"
 import Menu from "../components/Home/Menu"
 import Products from "../components/Home/Products"
 import Contact from "../components/Home/Contact"
@@ -17,7 +17,7 @@ const IndexPage = ({ data }) => (
       title="Regular Coffee"
       backgroundStyle="default-background"
     />
-    <Info />
+    {/* <Info /> */}
     <Menu items={data.menu} />
     <Products />
     <Contact />
